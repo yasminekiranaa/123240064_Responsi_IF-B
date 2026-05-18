857c00de6a1c94e43f4bc628e518992a79c9a666
Yasmine Kirana <aisyahyasmine96@gmail.com>
2026-05-18 14:56:27 +0700

    Update MahasiswaView.java

--- MahasiswaView.java
+++ MahasiswaView.java
@@ -9,5 +9,44 @@ package view;
  * @author Lab Informatika
  */
 public class MahasiswaView {
+    insert mahasiswa{
+        String sql = "INSERT INTO mahasiswa VALUES(NULL, ?, ?, ?)";
+
+        PreparedStatement ps = config.prepareStatement(sql);
+
+        ps.setint(1, id);
+        ps.setString(2, nama);
+        ps.setString(3, nim);
+
+        ps.execute update();
+    };
+     
+     update mahasiswa{
+        String sql = "UPDATE mahasiswa SET nama=?, nidn=? WHERE id=?";
+        
+        PreparedStatement ps = config.prepareStatement(sql);
+        
+        ps.setint(1, id);
+        ps.setString(2, nama);
+        ps.setString(3, nim);
+
+        ps.execute update();
+    };
+     
+    delete mahasiswa{
+        String sql = "DELETE FROM mahasiswa WHERE id=?";
+        
+        PreparedStatement ps = config.prepareStatement(sql);
+        
+        ps.setint(1, id);
+        
+        ps.execute update();
+    }
     
+    search mahasiswa{
+        String sql = "SELECT * FROM mahasiswa WHERE nama LIKE ? OR nim IKE ?";
+        
+        ps.setString(1, "%" + keyword + "%");
+        ps.setString(2, "%" + keyword + "%");
+    }
 }
