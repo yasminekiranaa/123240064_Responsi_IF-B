8f75393c8def0d795079161e39b1acf8889a2ba6
Yasmine Kirana <aisyahyasmine96@gmail.com>
2026-05-18 14:25:28 +0700

    Update Users.java

--- Users.java
+++ Users.java
@@ -9,5 +9,30 @@ package model;
  * @author Lab Informatika
  */
 public class Users {
+    private Integer id;
+    private String username, password;
     
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
