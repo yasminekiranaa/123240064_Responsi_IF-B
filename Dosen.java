312b52010655742c0a0359c918706bd0e33a0430
Yasmine Kirana <aisyahyasmine96@gmail.com>
2026-05-18 14:22:49 +0700

    Update Dosen.java

--- Dosen.java
+++ Dosen.java
@@ -9,5 +9,30 @@ package model;
  * @author Lab Informatika
  */
 public class Dosen {
+    private Integer id;
+    private String nama, nidn;
     
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNidn() {
+        return nidn;
+    }
+
+    public void setNidn(String nim) {
+        this.nidn = nidn;
+    }
 }
