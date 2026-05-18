ca5500840664a73223c92f0e5152c66adea316ea
Yasmine Kirana <aisyahyasmine96@gmail.com>
2026-05-18 14:08:35 +0700

    Create MenuView.java

---
+++ MenuView.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package view;
+
+/**
+ *
+ * @author Lab Informatika
+ */
+public class MenuView {
+    JLabel label = new JLabel (
+            "Selamat Datang, " + username + "!"
+    );
+    
+    JButton btnDosen = new JButton("Data Dosen");
+    
+    JButton btnMahasiswa = new JButton("Data Mahasiswa");
+}
