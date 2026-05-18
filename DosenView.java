edecc963d451480107a35e502d506c5c6628a1ac
Yasmine Kirana <aisyahyasmine96@gmail.com>
2026-05-18 14:54:30 +0700

    Update DosenView.java

--- DosenView.java
+++ DosenView.java
@@ -9,5 +9,44 @@ package view;
  * @author Lab Informatika
  */
 public class DosenView {
+     insert dosen{
+        String sql = "INSERT INTO dosen VALUES(NULL, ?, ?, ?)";
+
+        PreparedStatement ps = config.prepareStatement(sql);
+
+        ps.setint(1, id);
+        ps.setString(2, nama);
+        ps.setString(3, nidn);
+
+        ps.execute update();
+    };
+     
+     update dosen{
+        String sql = "UPDATE dosen SET nama=?, nidn=? WHERE id=?";
+        
+        PreparedStatement ps = config.prepareStatement(sql);
+        
+        ps.setint(1, id);
+        ps.setString(2, nama);
+        ps.setString(3, nidn);
+
+        ps.execute update();
+    };
+     
+    delete dosen{
+        String sql = "DELETE FROM dosen WHERE id=?";
+        
+        PreparedStatement ps = config.prepareStatement(sql);
+        
+        ps.setint(1, id);
+        
+        ps.execute update();
+    }
     
+    search dosen{
+        String sql = "SELECT * FROM dosen WHERE nama LIKE ? OR nidn IKE ?";
+        
+        ps.setString(1, "%" + keyword + "%");
+        ps.setString(2, "%" + keyword + "%");
+    }
 }
