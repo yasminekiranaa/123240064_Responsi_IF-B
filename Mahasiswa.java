327ac5b8d544386ad5e0d914675e6810113f4f33
Yasmine Kirana <aisyahyasmine96@gmail.com>
2026-05-18 14:20:05 +0700

    Update Mahasiswa.java

--- Mahasiswa.java
+++ Mahasiswa.java
@@ -8,6 +8,32 @@ package model;
  *
  * @author Lab Informatika
  */
+
 public class Mahasiswa {
+    private Integer id;
+    private String nama, nim;
     
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNim() {
+        return nim;
+    }
+
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
 }
