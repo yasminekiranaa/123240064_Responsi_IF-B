62b5e4135c5d9722b6f053fc6cef4bb51ece0c8c
Yasmine Kirana <aisyahyasmine96@gmail.com>
2026-05-18 15:09:03 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,7 @@
+
+import controller.LoginController;
+import view.LoginView;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
